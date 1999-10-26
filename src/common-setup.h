260c7928045e2a632d4fb325fb0f71d638499874
Timo Sirainen <cras@irssi.org>
1999-10-26 21:34:58 +0000

    Changed setting code behaviour. setup_get_str(), setup_get_int() and setup_get_bool() should be used instead of directly accessing setup variables. This has the advantage of not requiring to build the whole irssi every time when adding new configuration variable.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@64 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/common-setup.h
+++ src/common-setup.h
@@ -1,7 +1,8 @@
 #ifndef __COMMON_SETUP_H
 #define __COMMON_SETUP_H
 
-#define DCC_FILE_CREATE_MODE 0644
+#include "settings/settings-public.h"
+
 #define LOG_FILE_CREATE_MODE 0644
 #define CMD_CHAR '/'
 
@@ -23,80 +24,6 @@
 /* lists */
 extern GList *aliases, *ignores, *completions, *notifies, *hilights;
 
-/* look and feel */
-extern gboolean toggle_show_menubar;
-extern gboolean toggle_show_toolbar;
-extern gboolean toggle_show_statusbar;
-extern gboolean toggle_show_nicklist;
-extern gboolean toggle_show_timestamps;
-extern gboolean toggle_hide_text_style;
-extern gboolean toggle_bell_beeps;
-extern gboolean toggle_actlist_moves;
-extern gboolean toggle_privmsg_beeps;
-
-extern gboolean toggle_use_status_window;
-extern gboolean toggle_use_msgs_window;
-extern gboolean toggle_autoraise_msgs_window;
-extern gboolean toggle_autocreate_query;
-extern gboolean toggle_notifylist_popups;
-extern gboolean toggle_use_tabbed_windows;
-extern gint tab_orientation;
-
-/* misc */
-extern gchar *url_www_client;
-extern gchar *url_ftp_client;
-extern gchar *url_mail_client;
-
-extern gchar *ctcp_version_reply;
-extern gchar *default_quit_message;
-extern gchar *default_user_mode;
-
-extern gint min_lag_check_time;
-extern gint max_lag_before_disconnect;
-
-extern gint knockout_time; /* How many seconds to keep /knockouted ban */
-extern gboolean check_irssi_versions; /* Check if there's new irssi version available */
-
-/* history */
-extern gint max_command_history;
-extern gint max_textwidget_lines;
-extern gint rawlog_lines;
-extern gint block_remove_lines;
-
-extern gboolean toggle_window_history;
-
-/* nick completion */
-extern gchar *completion_char;
-extern gboolean completion_disable_auto;
-extern gint completion_keep_publics;
-extern gint completion_keep_ownpublics;
-extern gint completion_keep_privates;
-
-/* flood protection */
-extern gint flood_timecheck; /* Flood check timeout */
-extern gint flood_max_msgs; /* Max msgs in FLOOD_TIMECHECK msecs before considered as flooding */
-extern gint autoignore_time; /* How many seconds to keep someone autoignored */
-extern gint ctcp_timecheck; /* CTCP reply send timeout */
-extern gint max_ctcp_queue; /* Max CTCP reply queue length */
-extern gint cmd_queue_speed; /* Minimum timeout before sending the next command to server */
-
-/* dcc */
-extern gboolean toggle_dcc_autodisplay_dialog;
-extern gboolean toggle_dcc_autoget;
-extern gboolean toggle_dcc_autorename;
-extern gint dcc_max_autoget_size;
-extern gchar *dcc_autoget_masks;
-extern gchar *dcc_autochat_masks;
-extern gchar *dcc_download_path;
-
-extern gboolean toggle_dcc_fast_send;
-extern gchar *dcc_upload_path;
-
-extern gboolean toggle_dcc_mirc_ctcp;
-extern gint dcc_block_size;
-extern gint dcc_port;
-extern gint dcc_timeout;
-
 /* servers */
 typedef struct
 {
@@ -112,12 +39,9 @@ SETUP_SERVER_REC;
 
 extern GList *setupservers; /* list of local servers */
 extern GList *ircnets; /* list of available ircnets */
-extern gint server_reconnect_time; /* reconnect to server no sooner than n seconds */
-extern gchar *source_host; /* Our own IP to use */
-extern gboolean source_host_ok; /* Use source_host_ip .. */
+
 extern IPADDR source_host_ip; /* Resolved address */
-extern gchar *default_nick, *alternate_nick, *user_name, *real_name;
-extern gboolean toggle_skip_motd;
+extern gboolean source_host_ok; /* Use source_host_ip .. */
 
 /* channels */
 typedef struct
@@ -136,17 +60,5 @@ typedef struct
 SETUP_CHANNEL_REC;
 
 extern GList *setupchannels;
-
-/* IRC proxy */
-extern gboolean toggle_use_ircproxy;
-extern gchar *proxy_address;
-extern gint proxy_port;
-extern gchar *proxy_string;
-
-/* appearance */
-extern gboolean toggle_buggy_gtkthemes;
-extern gboolean toggle_use_itext;
-extern gboolean toggle_background_transparent;
-extern gint panel_max_channels;
 
 #endif
