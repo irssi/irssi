4d584944436bc68beff2b0f4f8f0542769a91ba5
Timo Sirainen <cras@irssi.org>
1999-10-30 13:21:14 +0000

    Internationalization works now.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@67 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- acconfig.h
+++ acconfig.h
@@ -1,24 +1,26 @@
+/* paths */
 #undef SYSCONFDIR
+#undef HELPDIR
 #undef PLUGINSDIR
-#undef PERLSCRIPTDIR
+#undef LOCALEDIR
 
+/* gtk / gnome */
 #undef HAVE_GTK
 #undef HAVE_IMLIB
 #undef HAVE_GNOME
 #undef HAVE_GNOME_PANEL
 
+/* hebrew support for GtkText */
 #undef GTK_HEBREW
 #undef GTK_HEBREW_RC
 
+/* misc.. */
 #undef MEM_DEBUG
 #undef HAVE_IPV6
-
 #undef HAVE_POPT_H
 #undef HAVE_SOCKS_H
 
-#undef HAVE_NCURSES_USE_DEFAULT_COLORS
-#undef HAVE_CURSES_IDCOK
-
+/* macros/curses checks */
 #undef HAS_CURSES
 #undef USE_SUNOS_CURSES
 #undef USE_BSD_CURSES
@@ -27,3 +29,14 @@
 #undef NO_COLOR_CURSES
 #undef SCO_FLAVOR
 #undef NCURSES_970530
+
+/* our own curses checks */
+#undef HAVE_NCURSES_USE_DEFAULT_COLORS
+#undef HAVE_CURSES_IDCOK
+
+/* nls */
+#undef ENABLE_NLS
+#undef HAVE_CATGETS
+#undef HAVE_GETTEXT
+#undef HAVE_LC_MESSAGES
+#undef HAVE_STPCPY
