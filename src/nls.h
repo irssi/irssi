accc66c5118d9d5150e978285fda1ff0a568ca86
Timo Sirainen <cras@irssi.org>
2000-01-12 22:10:30 +0000

    updates for new version:
    + configure displays a summary of things to compile
    + /set toggle_use_colors = yes|no, sets colors on/off in irssi-text
    - Some kB/s messages displayed wrong values when resuming DCC
      transfers. Also, kB/s is now displayed with two decimals
    - "Day changed to 00-10-2000" .. month was wrong. No Y2K bugs
      however ;)
    - List of ircnets was displyed wrong in server dialog.
    - Userhost replies didn't handle ircops right..
    - Doesn't quit when receives SIGHUP - some window managers send it
      when restarting itself (Afterstep)
    - Specifying "source host IP" didn't work (vhosts).
    - Using ctrl-b etc. didn't move the cursor forward..
    - Don't try to compile GTK parts of plugins if we don't even want
      build GTK irssi
    - Doesn't crash when trying to create DCC dialog after being
      disconnected from IRC server
    
    
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@104 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/nls.h
+++ src/nls.h
@@ -1,18 +1,22 @@
 #ifdef ENABLE_NLS
+#  ifdef HAVE_LIBINTL_H
 #    include <libintl.h>
-#    define _(String) gettext (String)
-#    ifdef gettext_noop
-#        define N_(String) gettext_noop (String)
-#    else
-#        define N_(String) (String)
-#    endif
+#  else
+#    include "../intl/libgettext.h"
+#  endif
+#  define _(String) gettext (String)
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop (String)
+#  else
+#    define N_(String) (String)
+#  endif
 #else
 /* Stubs that do something close enough.  */
-#    define textdomain(String) (String)
-#    define gettext(String) (String)
-#    define dgettext(Domain,Message) (Message)
-#    define dcgettext(Domain,Message,Type) (Message)
-#    define bindtextdomain(Domain,Directory) (Domain)
-#    define _(String) (String)
-#    define N_(String) (String)
+#  define textdomain(String) (String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,Message) (Message)
+#  define dcgettext(Domain,Message,Type) (Message)
+#  define bindtextdomain(Domain,Directory) (Domain)
+#  define _(String) (String)
+#  define N_(String) (String)
 #endif
