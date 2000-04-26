d01b094151705d433bc43cae9eeb304e6f110a17
Timo Sirainen <cras@irssi.org>
2000-04-26 07:50:02 +0000

    ..still removing..
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@170 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/irssi-plugin.h
+++
@@ -1,9 +0,0 @@
-/* This contains all the necessary includes for irssi plugins.
-   irc-extra/plugins.h and irc-extra/plugins-commands.h has all the plugin
-   management functions */
-
-#include "common.h"
-#include "irc-base/irc-base.h"
-#include "irc-extra/irc-extra.h"
-#include "ui-common/ui-common.h"
-#include "settings/settings.h"
