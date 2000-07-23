1a655f357598c9d9f3d2ddbe61a10803ffc73d5b
Timo Sirainen <cras@irssi.org>
2000-07-23 11:54:54 +0000

    is_numeric() returns now FALSE if it was given empty string.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@514 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/misc.c
+++ src/core/misc.c
@@ -421,6 +421,9 @@ int is_numeric(const char *str, char end_char)
 {
 	g_return_val_if_fail(str != NULL, FALSE);
 
+	if (*str == '\0' || *str == end_char)
+		return FALSE;
+
 	while (*str != '\0' && *str != end_char) {
 		if (!isdigit(*str)) return FALSE;
 		str++;
