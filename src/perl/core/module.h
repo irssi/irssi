f8ea75ceb144fd7efaf0c582f10d06a05420235f
Timo Sirainen <cras@irssi.org>
2000-10-01 21:56:20 +0000

    added
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@709 dbcabf3a-b0e7-0310-adc4-f8d773084564

---
+++ src/perl/core/module.h
@@ -0,0 +1,37 @@
+#include <EXTERN.h>
+#include <perl.h>
+#include <XSUB.h>
+
+#undef _
+#include "common.h"
+#include "network.h"
+#include "commands.h"
+#include "log.h"
+#include "rawlog.h"
+#include "settings.h"
+#include "masks.h"
+
+#include "servers.h"
+#include "servers-reconnect.h"
+#include "servers-setup.h"
+#include "channels.h"
+#include "queries.h"
+#include "nicklist.h"
+
+#include "perl/perl-common.h"
+
+#include "fe-common/core/windows.h"
+
+typedef COMMAND_REC *Irssi__Command;
+typedef LOG_REC *Irssi__Log;
+typedef RAWLOG_REC *Irssi__Rawlog;
+
+typedef SERVER_REC *Irssi__Server;
+typedef SERVER_CONNECT_REC *Irssi__Connect;
+typedef RECONNECT_REC *Irssi__Reconnect;
+typedef CHANNEL_REC *Irssi__Channel;
+typedef QUERY_REC *Irssi__Query;
+typedef NICK_REC *Irssi__Nick;
+
+typedef WINDOW_REC *Irssi__Window;
+typedef WI_ITEM_REC *Irssi__Windowitem;
