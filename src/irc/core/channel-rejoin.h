3977308cda8dd4a97d48be4cf58e6f00c29e59ee
Timo Sirainen <cras@irssi.org>
2000-10-14 01:57:27 +0000

    added
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@746 dbcabf3a-b0e7-0310-adc4-f8d773084564

---
+++ src/irc/core/channel-rejoin.h
@@ -0,0 +1,13 @@
+#ifndef __CHANNEL_REJOIN_H
+#define __CHANNEL_REJOIN_H
+
+typedef struct {
+	char *channel;
+	char *key;
+	int joining:1;
+} REJOIN_REC;
+
+void channel_rejoin_init(void);
+void channel_rejoin_deinit(void);
+
+#endif
