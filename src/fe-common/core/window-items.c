b3b1438d22a437d472107957db8ac0a949c52d58
Timo Sirainen <cras@irssi.org>
2000-11-09 20:38:04 +0000

    when finding window for item, prefer empty windows that don't have anything waiting channels list to those that have.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@818 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/core/window-items.c
+++ src/fe-common/core/window-items.c
@@ -253,7 +253,8 @@ void window_item_create(WI_ITEM_REC *item, int automatic)
 
 		if (reuse_unused_windows &&
 		    rec->items == NULL && rec->level == 0 &&
-		    (window == NULL || rec == active_win)) {
+		    (window == NULL || rec == active_win ||
+		     window->waiting_channels == NULL)) {
                         /* no items in this window, we should probably use it.. */
 			window = rec;
 		}
