5627e7a822c04d1d764506b3ed9498b4557d8b81
Timo Sirainen <cras@irssi.org>
2001-02-23 12:08:53 +0000

    nick_nfind(): gets stuck to endless loop if there's multiple identical nicks in channel.. Luckily this is no problem with IRC.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@1299 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/nicklist.c
+++ src/core/nicklist.c
@@ -451,6 +451,7 @@ static NICK_REC *nick_nfind(CHANNEL_REC *channel, const char *nick, int len)
 		while (rec->next != NULL) {
 			if (strcmp(rec->nick, tmpnick) == 0)
 				break;
+                        rec = rec->next;
 		}
 	}
 
