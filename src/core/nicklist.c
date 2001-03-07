00abe69923f124555f50e129249acb7d4265fea7
Timo Sirainen <cras@irssi.org>
2001-03-07 23:55:33 +0000

    removing second last unique nick didn't work properly
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@1359 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/nicklist.c
+++ src/core/nicklist.c
@@ -63,7 +63,7 @@ static void nick_hash_remove(CHANNEL_REC *channel, NICK_REC *nick)
 	if (list->next == NULL)
 		g_hash_table_remove(channel->nicks, nick->nick);
 	else if (list == nick) {
-		g_hash_table_insert(channel->nicks, nick->nick,
+		g_hash_table_insert(channel->nicks, nick->next->nick,
 				    nick->next);
 	} else {
 		while (list->next != nick)
