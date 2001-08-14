ed93f91d96fdb9dbdbc3d08c45887dbca087297e
Timo Sirainen <cras@irssi.org>
2001-08-14 11:22:19 +0000

    Typofix - might have caused a crash if channel was destroyed with /WINDOW CLOSE.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@1753 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/core/chat-completion.c
+++ src/fe-common/core/chat-completion.c
@@ -162,7 +162,7 @@ static void sig_message_join(SERVER_REC *server, const char *channel,
 	CHANNEL_REC *chanrec;
 
 	chanrec = channel_find(server, channel);
-	if (channel != NULL)
+	if (chanrec != NULL)
 		CHANNEL_LAST_MSG_ADD(chanrec, nick, FALSE);
 }
 
