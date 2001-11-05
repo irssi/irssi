6197339b8df2a657c0707c15c96ba44930541265
Timo Sirainen <cras@irssi.org>
2001-11-05 11:47:52 +0000

    /CHANNEL xxx - don't crash if not connected to server.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@1973 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/core/fe-channels.c
+++ src/fe-common/core/fe-channels.c
@@ -236,7 +236,7 @@ static void cmd_channel(const char *data, SERVER_REC *server, WI_ITEM_REC *item)
 {
 	if (*data == '\0')
 		cmd_channel_list_joined();
-	else if (server_ischannel(server, data)) {
+	else if (server != NULL && server_ischannel(server, data)) {
 		signal_emit("command join", 3, data, server, item);
 	} else {
 		command_runsub("channel", data, server, item);
