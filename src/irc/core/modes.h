94ad289e7c311f4acd6b5c99773712cf3e7c569e
Timo Sirainen <cras@irssi.org>
2001-11-17 15:59:33 +0000

    added +q channel mode to HAS_MODE_ARG_ALWAYS() list.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@2043 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/irc/core/modes.h
+++ src/irc/core/modes.h
@@ -3,8 +3,9 @@
 
 /* modes that have argument always */
 #define HAS_MODE_ARG_ALWAYS(mode) \
-	((mode) == 'b' || (mode) == 'e' || (mode) == 'I' || (mode) == 'k' || \
-	(mode) == 'o' || (mode) == 'h' || (mode) == 'v' || (mode) == 'O')
+	((mode) == 'b' || (mode) == 'e' || (mode) == 'I' || (mode) == 'q' || \
+        (mode) == 'o' || (mode) == 'h' || (mode) == 'v' || (mode) == 'O' || \
+	(mode) == 'k')
 
 /* modes that have argument when being set (+) */
 #define HAS_MODE_ARG_SET(mode) \
