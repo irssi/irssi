cd09cd63329f43b330593b8b6602deffcd71e40c
Timo Sirainen <cras@irssi.org>
2001-11-20 20:54:21 +0000

    Still forgot to add pidwait_get_pids() here.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@2133 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/pidwait.h
+++ src/core/pidwait.h
@@ -9,4 +9,8 @@ void pidwait_add(int pid);
 /* remove pid from wait list */
 void pidwait_remove(int pid);
 
+/* return list of pids that are being waited.
+   don't free the return value. */
+GSList *pidwait_get_pids(void);
+
 #endif
