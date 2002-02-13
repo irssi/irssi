f37ea1af1e52127c9967b54705d72fa80a9ac535
Timo Sirainen <cras@irssi.org>
2002-02-13 15:08:28 +0000

    updated the glib error message with netsplits
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@2436 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/irc/core/netsplit.c
+++ src/irc/core/netsplit.c
@@ -103,7 +103,10 @@ static NETSPLIT_REC *netsplit_add(IRC_SERVER_REC *server, const char *nick,
 	g_return_val_if_fail(nick != NULL, NULL);
 	g_return_val_if_fail(address != NULL, NULL);
 
-	g_return_val_if_fail(g_hash_table_lookup(server->splits, nick) == NULL, NULL);
+	if (g_hash_table_lookup(server->splits, nick) != NULL) {
+		g_warning("%s is already in split list (how?)", nick);
+                return NULL;
+	}
 
 	/* get splitted servers */
 	dupservers = g_strdup(servers);
