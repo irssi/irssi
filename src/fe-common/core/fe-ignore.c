86ab5ade862acdd4f16a0e5f4c05587849be656b
Timo Sirainen <cras@irssi.org>
2002-05-18 07:48:23 +0000

    /IGNORE -pattern now always assumes you want to create a new ignore. If you wish to update existing, /UNIGNORE it first.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@2814 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/core/fe-ignore.c
+++ src/fe-common/core/fe-ignore.c
@@ -134,7 +134,7 @@ static void cmd_ignore(const char *data)
 	channels = (chanarg == NULL || *chanarg == '\0') ? NULL :
 		g_strsplit(replace_chars(chanarg, ',', ' '), " ", -1);
 
-	rec = ignore_find(NULL, mask, channels);
+	rec = patternarg != NULL ? NULL: ignore_find(NULL, mask, channels);
 	new_ignore = rec == NULL;
 
 	if (rec == NULL) {
