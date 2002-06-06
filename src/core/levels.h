357bd2f1a4bbff6d634b18ea4a593d3e7d61d989
Timo Sirainen <cras@irssi.org>
2002-06-06 20:43:57 +0000

    Levels are now defined as enums, not #defines.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@2846 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/levels.h
+++ src/core/levels.h
@@ -7,35 +7,37 @@
    needed..). */
 
 /* Message levels */
-#define MSGLEVEL_CRAP         0x0000001
-#define MSGLEVEL_MSGS         0x0000002
-#define MSGLEVEL_PUBLIC       0x0000004
-#define MSGLEVEL_NOTICES      0x0000008
-#define MSGLEVEL_SNOTES       0x0000010
-#define MSGLEVEL_CTCPS        0x0000020
-#define MSGLEVEL_ACTIONS      0x0000040
-#define MSGLEVEL_JOINS        0x0000080
-#define MSGLEVEL_PARTS        0x0000100
-#define MSGLEVEL_QUITS        0x0000200
-#define MSGLEVEL_KICKS        0x0000400
-#define MSGLEVEL_MODES        0x0000800
-#define MSGLEVEL_TOPICS       0x0001000
-#define MSGLEVEL_WALLOPS      0x0002000
-#define MSGLEVEL_INVITES      0x0004000
-#define MSGLEVEL_NICKS        0x0008000
-#define MSGLEVEL_DCC          0x0010000
-#define MSGLEVEL_DCCMSGS      0x0020000
-#define MSGLEVEL_CLIENTNOTICE 0x0040000
-#define MSGLEVEL_CLIENTCRAP   0x0080000
-#define MSGLEVEL_CLIENTERROR  0x0100000
-#define MSGLEVEL_HILIGHT      0x0200000
+enum {
+	MSGLEVEL_CRAP         = 0x0000001,
+	MSGLEVEL_MSGS         = 0x0000002,
+	MSGLEVEL_PUBLIC       = 0x0000004,
+	MSGLEVEL_NOTICES      = 0x0000008,
+	MSGLEVEL_SNOTES       = 0x0000010,
+	MSGLEVEL_CTCPS        = 0x0000020,
+	MSGLEVEL_ACTIONS      = 0x0000040,
+	MSGLEVEL_JOINS        = 0x0000080,
+	MSGLEVEL_PARTS        = 0x0000100,
+	MSGLEVEL_QUITS        = 0x0000200,
+	MSGLEVEL_KICKS        = 0x0000400,
+	MSGLEVEL_MODES        = 0x0000800,
+	MSGLEVEL_TOPICS       = 0x0001000,
+	MSGLEVEL_WALLOPS      = 0x0002000,
+	MSGLEVEL_INVITES      = 0x0004000,
+	MSGLEVEL_NICKS        = 0x0008000,
+	MSGLEVEL_DCC          = 0x0010000,
+	MSGLEVEL_DCCMSGS      = 0x0020000,
+	MSGLEVEL_CLIENTNOTICE = 0x0040000,
+	MSGLEVEL_CLIENTCRAP   = 0x0080000,
+	MSGLEVEL_CLIENTERROR  = 0x0100000,
+	MSGLEVEL_HILIGHT      = 0x0200000,
 
-#define MSGLEVEL_ALL          0x03fffff
+	MSGLEVEL_ALL          = 0x03fffff,
 
-#define MSGLEVEL_NOHILIGHT    0x1000000 /* Don't highlight this message */
-#define MSGLEVEL_NO_ACT       0x2000000 /* Don't trigger channel activity */
-#define MSGLEVEL_NEVER        0x4000000 /* never ignore / never log */
-#define MSGLEVEL_LASTLOG      0x8000000 /* never ignore / never log */
+	MSGLEVEL_NOHILIGHT    = 0x1000000, /* Don't highlight this message */
+	MSGLEVEL_NO_ACT       = 0x2000000, /* Don't trigger channel activity */
+	MSGLEVEL_NEVER        = 0x4000000, /* never ignore / never log */
+	MSGLEVEL_LASTLOG      = 0x8000000 /* never ignore / never log */
+};
 
 int level_get(const char *level);
 int level2bits(const char *level);
