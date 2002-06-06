da1252cf2c3cdc8adf6827197346996754ceb50b
Timo Sirainen <cras@irssi.org>
2002-06-06 23:40:37 +0000

    /NAMES list isn't displayed on join when channel is joined because of /UPGRADE.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@2848 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/core/fe-channels.c
+++ src/fe-common/core/fe-channels.c
@@ -105,7 +105,8 @@ static void signal_window_item_changed(WINDOW_REC *window, WI_ITEM_REC *item)
 
 static void sig_channel_joined(CHANNEL_REC *channel)
 {
-	if (settings_get_bool("show_names_on_join"))
+	if (settings_get_bool("show_names_on_join") &&
+	    !channel->session_rejoin)
 		fe_channels_nicklist(channel, CHANNEL_NICKLIST_FLAG_ALL);
 }
 
