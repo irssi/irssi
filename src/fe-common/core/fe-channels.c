777349ecc1685772fd25d315420872ae5375c472
Timo Sirainen <cras@irssi.org>
2002-09-18 17:02:58 +0000

    !channels weren't rejoined to correct window after reconnect/cycle.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@2928 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/core/fe-channels.c
+++ src/fe-common/core/fe-channels.c
@@ -86,7 +86,7 @@ static void sig_disconnected(SERVER_REC *server)
 		CHANNEL_REC *channel = tmp->data;
 
 		window = window_item_window((WI_ITEM_REC *) channel);
-		window_bind_add(window, server->tag, channel->visible_name);
+		window_bind_add(window, server->tag, channel->name);
 	}
 }
 
