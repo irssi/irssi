e3c041955f6872a72ee1d1c9617ca5e55a3448d1
Timo Sirainen <cras@irssi.org>
2002-11-17 15:28:40 +0000

    cmd_get_quoted_param() didn't work properly if it was used with last word (ie. like ended with '"', which broke eg. /dcc send "file")
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@3005 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/commands.c
+++ src/core/commands.c
@@ -497,7 +497,8 @@ char *cmd_get_quoted_param(char **data)
 	quote = **data; (*data)++;
 
 	pos = *data;
-	while (**data != '\0' && (**data != quote || (*data)[1] != ' ')) {
+	while (**data != '\0' && (**data != quote ||
+				  ((*data)[1] != ' ' && (*data)[1] != '\0'))) {
 		if (**data == '\\' && (*data)[1] != '\0')
                         g_memmove(*data, (*data)+1, strlen(*data));
 		(*data)++;
