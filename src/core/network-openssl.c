07642119986c5b951656d9ebb844940b877298df
Timo Sirainen <cras@irssi.org>
2003-11-16 19:40:04 +0000

    compile fix if there's no openssl
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@3154 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/network-openssl.c
+++ src/core/network-openssl.c
@@ -548,7 +548,7 @@ GIOChannel *net_connect_ip_ssl(IPADDR *ip, int port, IPADDR *my_ip, const char *
 
 #else /* HAVE_OPENSSL */
 
-GIOChannel *net_connect_ip_ssl(IPADDR *ip, int port, IPADDR *my_ip)
+GIOChannel *net_connect_ip_ssl(IPADDR *ip, int port, IPADDR *my_ip, const char *cert, const char *pkey, const char *cafile, const char *capath, gboolean verify)
 {
 	g_warning("Connection failed: SSL support not enabled in this build.");
 	errno = ENOSYS;
