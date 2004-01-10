f380cef4dd7db814322e87f2302882cd94e0bf40
Timo Sirainen <cras@irssi.org>
2004-01-10 16:33:48 +0000

    fix
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@3200 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/core/chat-completion.c
+++ src/fe-common/core/chat-completion.c
@@ -219,7 +219,6 @@ static void sig_message_own_private(SERVER_REC *server, const char *msg,
 				    const char *target, const char *origtarget)
 {
 	g_return_if_fail(server != NULL);
-	g_return_if_fail(target != NULL);
 
 	if (target != NULL && query_find(server, target) == NULL)
 		SERVER_LAST_MSG_ADD(server, target);
