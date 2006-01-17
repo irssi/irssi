df69be65d42cdcdd4fc2025042de72f5792e372d
Wouter Coekaerts <coekie@irssi.org>
2006-01-17 20:20:28 +0000

    fix segfault on /quit by using a linked list node after freeing it (by  Chris Moore)
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4202 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/irc/dcc/dcc.c
+++ src/irc/dcc/dcc.c
@@ -58,8 +58,8 @@ void dcc_unregister_type(const char *type)
 
 	pos = gslist_find_string(dcc_types, type);
 	if (pos != NULL) {
-                dcc_types = g_slist_remove(dcc_types, pos->data);
 		g_free(pos->data);
+                dcc_types = g_slist_remove(dcc_types, pos->data);
 	}
 }
 
