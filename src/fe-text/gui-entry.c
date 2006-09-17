8264b65939fcfca0020da485ee76d3a280a99625
Emanuele Giaquinta <exg@irssi.org>
2006-09-17 12:26:04 +0000

    Fix off-by-one error.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4352 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-text/gui-entry.c
+++ src/fe-text/gui-entry.c
@@ -242,7 +242,7 @@ static void gui_entry_draw_from(GUI_ENTRY_REC *entry, int pos)
 	}
 
         /* clear the rest of the input line */
-        if (end_xpos == term_width)
+        if (end_xpos == term_width-1)
 		term_clrtoeol(root_window);
 	else {
 		while (xpos < end_xpos) {
