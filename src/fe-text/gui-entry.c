e2cc91c19a82aae7f8349e19ea7e753c8fafae04
Emanuele Giaquinta <exg@irssi.org>
2007-04-14 20:20:37 +0000

    Modify gui_entry_draw_from to clear to end of line only if it has not written till the last column.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4449 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-text/gui-entry.c
+++ src/fe-text/gui-entry.c
@@ -242,6 +242,7 @@ static void gui_entry_draw_from(GUI_ENTRY_REC *entry, int pos)
 	}
 
         /* clear the rest of the input line */
+	if (xpos < end_xpos) {
         if (end_xpos == term_width)
 		term_clrtoeol(root_window);
 	else {
@@ -249,6 +250,7 @@ static void gui_entry_draw_from(GUI_ENTRY_REC *entry, int pos)
                         term_addch(root_window, ' ');
                         xpos++;
 		}
+	}
 	}
 }
 
