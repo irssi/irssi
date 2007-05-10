a7f2fc353522eecc76447b9ee18e18ed399cb872
Emanuele Giaquinta <exg@irssi.org>
2007-05-10 20:33:02 +0000

    Allow logging of lines with no target only to logs with no items or with a wildcard item.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4491 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/log.c
+++ src/core/log.c
@@ -299,9 +299,7 @@ void log_file_write(const char *server_tag, const char *item, int level,
 		if ((level & rec->level) == 0)
 			continue;
 
-		if (item == NULL) {
-			if (rec->temp == 0) log_write_rec(rec, str, level);
-		} else if (rec->items == NULL)
+		if (rec->items == NULL)
 			fallbacks = g_slist_append(fallbacks, rec);
 		else if (log_item_find(rec, LOG_ITEM_TARGET, item,
 				       server_tag) != NULL)
