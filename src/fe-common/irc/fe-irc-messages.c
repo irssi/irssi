9fd8510d873ed17b6416a5e5bb28ae5da7aa7ac0
Emanuele Giaquinta <exg@irssi.org>
2007-10-22 13:27:56 +0000

    Handle server == NULL case in skip_target, fix regression with irssi-icb. Patch by Stuart Henderson, bug #539.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4627 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-common/irc/fe-irc-messages.c
+++ src/fe-common/irc/fe-irc-messages.c
@@ -44,7 +44,7 @@ static const char *skip_target(IRC_SERVER_REC *server, const char *target)
 	const char *val, *chars;
 
 	/* Quick check */
-	if (server->prefix[(int)(unsigned char)*target] == 0)
+	if (server == NULL || server->prefix[(int)(unsigned char)*target] == 0)
 		return target;
 
 	/* Hack: for bahamut 1.4 which sends neither STATUSMSG nor
