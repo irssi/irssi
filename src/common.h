feab8e7503d4a63f1db2090dbf27d86be617ded0
Emanuele Giaquinta <exg@irssi.org>
2008-02-02 17:54:51 +0000

    Fix indentation.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4696 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/common.h
+++ src/common.h
@@ -20,8 +20,8 @@
 #include <stddef.h>
 #include <stdarg.h>
 #include <ctype.h>
-#  ifdef HAVE_STRING_H
-#include <string.h>
+#ifdef HAVE_STRING_H
+#  include <string.h>
 #endif
 #ifdef HAVE_STDLIB_H
 #  include <stdlib.h>
