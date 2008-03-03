65f43a705c21c431aa3e1d6a62f34ebdf759bbb8
Emanuele Giaquinta <exg@irssi.org>
2008-03-03 12:33:17 +0000

    Group together declarations for string functions.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4719 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/misc.h
+++ src/core/misc.h
@@ -14,13 +14,6 @@ int g_timeval_cmp(const GTimeVal *tv1, const GTimeVal *tv2);
    if the difference is too large, the result might be invalid. */
 long get_timeval_diff(const GTimeVal *tv1, const GTimeVal *tv2);
 
-/* find `item' from a space separated `list' */
-int find_substr(const char *list, const char *item);
-/* return how many items `array' has */
-int strarray_length(char **array);
-/* return index of `item' in `array' or -1 if not found */
-int strarray_find(char **array, const char *item);
-
 GSList *gslist_find_string(GSList *list, const char *key);
 GSList *gslist_find_icase_string(GSList *list, const char *key);
 GList *glist_find_string(GList *list, const char *key);
@@ -36,15 +29,6 @@ char *gslist_to_string(GSList *list, const char *delimiter);
 /* save all keys in hash table to linked list - you shouldn't remove any
    items while using this list, use g_slist_free() after you're done with it */
 GSList *hashtable_get_keys(GHashTable *hash);
-
-/* strstr() with case-ignoring */
-char *stristr(const char *data, const char *key);
-
-/* like strstr(), but matches only for full words.
-   `icase' specifies if match is case sensitive */
-char *strstr_full_case(const char *data, const char *key, int icase);
-char *strstr_full(const char *data, const char *key);
-char *stristr_full(const char *data, const char *key);
 
 /* easy way to check if regexp matches */
 int regexp_match(const char *str, const char *regexp);
@@ -64,22 +48,9 @@ int g_istr_cmp(gconstpointer v, gconstpointer v2);
 /* Find `mask' from `data', you can use * and ? wildcards. */
 int match_wildcards(const char *mask, const char *data);
 
-/* Return TRUE if all characters in `str' are numbers.
-   Stop when `end_char' is found from string. */
-int is_numeric(const char *str, char end_char);
-
-/* replace all `from' chars in string to `to' chars. returns `str' */
-char *replace_chars(char *str, char from, char to);
-
 /* octal <-> decimal conversions */
 int octal2dec(int octal);
 int dec2octal(int decimal);
-
-/* string -> uoff_t */
-uoff_t str_to_uofft(const char *str);
-
-/* convert all low-ascii (<32) to ^<A..> combinations */
-char *show_lowascii(const char *str);
 
 /* Get time in human readable form with localtime() + asctime() */
 char *my_asctime(time_t t);
@@ -98,16 +69,46 @@ GSList *columns_sort_list(GSList *list, int rows);
    one after '\'. Returns the expanded character or -1 if error. */
 int expand_escape(const char **data);
 
-/* Escape all '"', "'" and '\' chars with '\' */
-char *escape_string(const char *str);
-
-/* Like strlcpy(), but return -1 if buffer was overflown, 0 if not. */
-int strocpy(char *dest, const char *src, size_t dstsize);
-
 int nearest_power(int num);
 
 /* Returns TRUE / FALSE */
 int parse_time_interval(const char *time, int *msecs);
 int parse_size(const char *size, int *bytes);
+
+/* Return TRUE if all characters in `str' are numbers.
+   Stop when `end_char' is found from string. */
+int is_numeric(const char *str, char end_char);
+
+/* Like strlcpy(), but return -1 if buffer was overflown, 0 if not. */
+int strocpy(char *dest, const char *src, size_t dstsize);
+
+/* strstr() with case-ignoring */
+char *stristr(const char *data, const char *key);
+
+/* like strstr(), but matches only for full words.
+   `icase' specifies if match is case sensitive */
+char *strstr_full_case(const char *data, const char *key, int icase);
+char *strstr_full(const char *data, const char *key);
+char *stristr_full(const char *data, const char *key);
+
+/* Escape all '"', "'" and '\' chars with '\' */
+char *escape_string(const char *str);
+
+/* convert all low-ascii (<32) to ^<A..> combinations */
+char *show_lowascii(const char *str);
+
+/* replace all `from' chars in string to `to' chars. returns `str' */
+char *replace_chars(char *str, char from, char to);
+
+/* return how many items `array' has */
+int strarray_length(char **array);
+/* return index of `item' in `array' or -1 if not found */
+int strarray_find(char **array, const char *item);
+
+/* string -> uoff_t */
+uoff_t str_to_uofft(const char *str);
+
+/* find `item' from a space separated `list' */
+int find_substr(const char *list, const char *item);
 
 #endif
