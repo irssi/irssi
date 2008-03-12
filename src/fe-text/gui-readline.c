e1713c17cbf59ad13682f01e9b7b1907ad3a6078
Emanuele Giaquinta <exg@irssi.org>
2008-03-12 09:59:35 +0000

    After last change paste_entry includes the key before the one that begins the pasting, so do not add the same key to paste_buffer, otherwise it will show up twice.
    git-svn-id: http://svn.irssi.org/repos/irssi/trunk@4760 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/fe-text/gui-readline.c
+++ src/fe-text/gui-readline.c
@@ -402,7 +402,6 @@ static int check_pasting(unichar key, int diff)
 		g_array_set_size(paste_buffer, 0);
 		if (prev_key != '\r' && prev_key != '\n') {
 			paste_keycount++;
-			g_array_append_val(paste_buffer, prev_key);
 		}
 	} else if (paste_state > 0 && diff > paste_detect_time &&
 		   paste_line_count == 0) {
