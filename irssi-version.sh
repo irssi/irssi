6aa167e5ac748b84c8a544c32f9c76c7ad597a71
Jilles Tjoelker <jilles@irssi.org>
2009-05-13 21:01:55 +0000

    Do not put a space between awk's -F and its argument. Solaris's /usr/bin/awk chokes on the space. Bug #672
    git-svn-id: file:///var/www/svn.irssi.org/SVN/irssi/trunk@5065 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- irssi-version.sh
+++ irssi-version.sh
@@ -5,9 +5,9 @@ if test -d "$1"/.svn ; then
 elif test -d "$1"/.git ; then
 	DATE=`GIT_DIR=$1/.git git log -1 --pretty=format:%ai HEAD`
 else
-	DATE=`awk -F '|' 'NR == 2{print substr($3, 2)}' "$1"/ChangeLog`
+	DATE=`awk -F'|' 'NR == 2{print substr($3, 2)}' "$1"/ChangeLog`
 fi
 VERSION_DATE=`echo $DATE | cut -f 1 -d ' ' | tr -d -`
-VERSION_TIME=`echo $DATE | cut -f 2 -d ' ' | awk -F : '{printf "%d", $1$2}'`
+VERSION_TIME=`echo $DATE | cut -f 2 -d ' ' | awk -F: '{printf "%d", $1$2}'`
 echo "#define IRSSI_VERSION_DATE $VERSION_DATE"
 echo "#define IRSSI_VERSION_TIME $VERSION_TIME"
