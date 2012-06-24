7ae491922905fbc800c6c389517a5fbcce6f5734
Emanuele Giaquinta <exg@irssi.org>
2012-06-24 09:50:08 +0000

    Do not set SSL_OP_ALL, it is not needed to disable SSLv2 and it can prevent connections to TLSv1.1 servers from working. Patch by pi-rho.
    git-svn-id: file:///var/www/svn.irssi.org/SVN/irssi/trunk@5216 dbcabf3a-b0e7-0310-adc4-f8d773084564

--- src/core/network-openssl.c
+++ src/core/network-openssl.c
@@ -410,7 +410,7 @@ static GIOChannel *irssi_ssl_get_iochannel(GIOChannel *handle, const char *hostn
 		g_error("Could not allocate memory for SSL context");
 		return NULL;
 	}
-	SSL_CTX_set_options(ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2);
+	SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
 
 	if (mycert && *mycert) {
 		char *scert = NULL, *spkey = NULL;
