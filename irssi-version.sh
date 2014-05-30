ff70e9f976c41d2ccac19f7d2a8994492d35da22
Alexander Færøy <ahf@0x90.dk>
2014-05-31 01:13:46 +0200

    Kill svn support from irssi-version.sh

--- irssi-version.sh
+++ irssi-version.sh
@@ -1,13 +1,9 @@
-#! /bin/sh
+#!/bin/sh
 
-if test -d "$1"/.svn ; then
-	DATE=`LC_ALL=C svn info "$1" | sed -n 's/^Last Changed Date: \(.*\)/\1/p'`
-elif test -d "$1"/.git ; then
-	DATE=`GIT_DIR=$1/.git git log -1 --pretty=format:%ai HEAD`
-else
-	DATE=`awk -F'|' 'NR == 2{print substr($3, 2)}' "$1"/ChangeLog`
-fi
+DATE=`GIT_DIR=$1/.git git log -1 --pretty=format:%ai HEAD`
+
 VERSION_DATE=`echo $DATE | cut -f 1 -d ' ' | tr -d -`
 VERSION_TIME=`echo $DATE | cut -f 2 -d ' ' | awk -F: '{printf "%d", $1$2}'`
+
 echo "#define IRSSI_VERSION_DATE $VERSION_DATE"
 echo "#define IRSSI_VERSION_TIME $VERSION_TIME"
