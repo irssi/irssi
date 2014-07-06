mauke/irssi-hax

add italics support; don't use standout for reverse

src/fe-common/core/formats.h
@@ -13,6 +13,7 @@
 #define GUI_PRINT_FLAG_NEWLINE       0x0080
 #define GUI_PRINT_FLAG_CLRTOEOL      0x0100
 #define GUI_PRINT_FLAG_MONOSPACE     0x0200
+#define GUI_PRINT_FLAG_ITALIC        0x0400
 
 #define MAX_FORMAT_PARAMS 10
 #define DEFAULT_FORMAT_ARGLIST_SIZE 200
@@ -128,6 +129,7 @@ void format_send_to_gui(TEXT_DEST_REC *dest, const char *text);
 #define FORMAT_STYLE_BOLD	(0x03 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_REVERSE	(0x04 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_INDENT	(0x05 + FORMAT_STYLE_SPECIAL)
+#define FORMAT_STYLE_ITALIC	(0x06 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_DEFAULTS	(0x07 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_CLRTOEOL	(0x08 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_MONOSPACE	(0x09 + FORMAT_STYLE_SPECIAL)

irssi/irssi

src/fe-common/core/formats.h
@@ -10,6 +10,7 @@
 #define GUI_PRINT_FLAG_BLINK         0x0008
 #define GUI_PRINT_FLAG_MIRC_COLOR    0x0010
 #define GUI_PRINT_FLAG_INDENT        0x0020
+#define GUI_PRINT_FLAG_ITALIC        0x0040
 #define GUI_PRINT_FLAG_NEWLINE       0x0080
 #define GUI_PRINT_FLAG_CLRTOEOL      0x0100
 #define GUI_PRINT_FLAG_MONOSPACE     0x0200
@@ -139,6 +140,7 @@ void format_send_to_gui(TEXT_DEST_REC *dest, const char *text);
 #define FORMAT_STYLE_BOLD	(0x03 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_REVERSE	(0x04 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_INDENT	(0x05 + FORMAT_STYLE_SPECIAL)
+#define FORMAT_STYLE_ITALIC	(0x06 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_DEFAULTS	(0x07 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_CLRTOEOL	(0x08 + FORMAT_STYLE_SPECIAL)
 #define FORMAT_STYLE_MONOSPACE	(0x09 + FORMAT_STYLE_SPECIAL)