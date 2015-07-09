a393c675400a41f7cc3df988c25242857da678a4
Ailin Nemui <ailin@linux.localdomain>
2015-07-09 14:20:30 +0200

    Introduce check for git commands in autogen
    This avoids a misleading compile error when users try to `make` an
    incomplete (or non-git) check-out.

--- irssi-version.sh
+++ irssi-version.sh
@@ -5,6 +5,12 @@ DATE=`GIT_DIR=$1/.git git log -1 --pretty=format:%ai HEAD`
 VERSION_DATE=`echo $DATE | cut -f 1 -d ' ' | tr -d -`
 VERSION_TIME=`echo $DATE | cut -f 2 -d ' ' | awk -F: '{printf "%d", $1$2}'`
 
+if test -z "$VERSION_DATE"; then
+    exec>&2
+    echo "**Error**: `basename "$0"` must be run in a git clone, cannot proceed."
+    exit 1
+fi
+
 echo "#define IRSSI_VERSION_DATE $VERSION_DATE"
 echo "#define IRSSI_VERSION_TIME $VERSION_TIME"
 
