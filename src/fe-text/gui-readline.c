38d372eccb3745a7734b8f8963ae572d1343c5b1
dequis <dx@dxzone.com.ar>
2015-12-12 01:17:48 -0300

    Disable timeout-based paste detection if paste_use_bracketed_mode is on

--- src/fe-text/gui-readline.c
+++ src/fe-text/gui-readline.c
@@ -737,7 +737,7 @@ static void sig_input(void)
 		if (paste_bracketed_mode) {
 			paste_bracketed_middle();
 
-		} else if (paste_detect_time > 0 && paste_buffer->len >= 3) {
+		} else if (!paste_use_bracketed_mode && paste_detect_time > 0 && paste_buffer->len >= 3) {
 			if (paste_timeout_id != -1)
 				g_source_remove(paste_timeout_id);
 			paste_timeout_id = g_timeout_add(paste_detect_time, paste_timeout, NULL);
