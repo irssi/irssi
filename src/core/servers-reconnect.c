9ea155f8df5fe9b3d67e8729f611b87a0c2a82c9
dequis <dx@dxzone.com.ar>
2016-06-12 20:28:06 -0300

    servers-reconnect: pass unix_socket attribute to new connection
    Trying to /reconnect unix sockets turned them into inet.

--- src/core/servers-reconnect.c
+++ src/core/servers-reconnect.c
@@ -190,6 +190,7 @@ server_connect_copy_skeleton(SERVER_CONNECT_REC *src, int connect_info)
 	dest->away_reason = g_strdup(src->away_reason);
 	dest->no_autojoin_channels = src->no_autojoin_channels;
 	dest->no_autosendcmd = src->no_autosendcmd;
+	dest->unix_socket = src->unix_socket;
 
 	dest->use_ssl = src->use_ssl;
 	dest->ssl_cert = g_strdup(src->ssl_cert);
