1f1868c532c4493e16f20925fc77f7d6f83b6d47
Ailin Nemui <ailin@z30a.localdomain>
2017-06-05 15:12:16 +0200

    Merge remote-tracking branch 'gitlab/security'
    (cherry picked from commit fb08fc7f1aa6b2e616413d003bf021612301ad55)

--- src/irc/dcc/dcc-get.c
+++ src/irc/dcc/dcc-get.c
@@ -382,6 +382,8 @@ int get_file_params_count(char **params, int paramcount)
 	if (*params[0] == '"') {
 		/* quoted file name? */
 		for (pos = 0; pos < paramcount-3; pos++) {
+			if (strlen(params[pos]) == 0)
+				continue;
 			if (params[pos][strlen(params[pos])-1] == '"' &&
 			    get_params_match(params, pos+1))
 				return pos+1;
@@ -427,6 +429,10 @@ static void ctcp_msg_dcc_send(IRC_SERVER_REC *server, const char *data,
         uoff_t size;
 	int p_id = -1;
 	int passive = FALSE;
+
+	if (addr == NULL) {
+		addr = "";
+	}
 
 	/* SEND <file name> <address> <port> <size> [...] */
 	/* SEND <file name> <address> 0 <size> <id> (DCC SEND passive protocol) */
