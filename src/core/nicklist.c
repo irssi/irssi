5e46c6dda0993aff058baba03726d4454139ccfe
Nei <ailin.nemui@gmail.com>
2017-07-05 14:47:30 +0000

    Merge branch 'security' into 'master'
    Security
    
    Closes #10
    
    See merge request !17
    
    (cherry picked from commit 5e26325317c72a04c1610ad952974e206384d291)

--- src/core/nicklist.c
+++ src/core/nicklist.c
@@ -54,22 +54,25 @@ static void nick_hash_add(CHANNEL_REC *channel, NICK_REC *nick)
 
 static void nick_hash_remove(CHANNEL_REC *channel, NICK_REC *nick)
 {
-	NICK_REC *list;
+	NICK_REC *list, *newlist;
 
 	list = g_hash_table_lookup(channel->nicks, nick->nick);
 	if (list == NULL)
 		return;
 
-	if (list == nick || list->next == NULL) {
-		g_hash_table_remove(channel->nicks, nick->nick);
-		if (list->next != NULL) {
-			g_hash_table_insert(channel->nicks, nick->next->nick,
-					    nick->next);
-		}
+	if (list == nick) {
+		newlist = nick->next;
 	} else {
+		newlist = list;
 		while (list->next != nick)
 			list = list->next;
 		list->next = nick->next;
+	}
+
+	g_hash_table_remove(channel->nicks, nick->nick);
+	if (newlist != NULL) {
+		g_hash_table_insert(channel->nicks, newlist->nick,
+				    newlist);
 	}
 }
 
