beb2beba3b4802c6969a5595197e25e7a5483fa3
Joseph Bisch <joseph.bisch@gmail.com>
2017-10-18 14:33:02 -0400

    Revert "Don't proceed with cmd_msg if there was an error splitting msg"
    This reverts commit bd83852d646de28f2e0fe01efe7c9236aa4074d4.

--- src/core/chat-commands.c
+++ src/core/chat-commands.c
@@ -404,10 +404,7 @@ static void cmd_msg(const char *data, SERVER_REC *server, WI_ITEM_REC *item)
 		else
 			splitmsgs = singlemsg;
 
-		/* splitmsgs may be NULL if there was an error */
-		g_warn_if_fail(splitmsgs != NULL);
-
-		while (splitmsgs && (m = splitmsgs[n++])) {
+		while ((m = splitmsgs[n++])) {
 			signal_emit("server sendmsg", 4, server, target, m,
 				    GINT_TO_POINTER(target_type));
 			signal_emit(target_type == SEND_TARGET_CHANNEL ?
