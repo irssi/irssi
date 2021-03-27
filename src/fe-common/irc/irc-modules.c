be8758f6cb893b74107582b3264958a736776dce
Ailin Nemui <ailin@d5401s.localdomain>
2021-03-27 11:05:54 +0100

    autoload the IRC module

--- src/fe-common/irc/irc-modules.c
+++ src/fe-common/irc/irc-modules.c
@@ -1,4 +1,6 @@
+#ifdef HAVE_STATIC_IRC
 void fe_irc_dcc_init(void);void fe_irc_notifylist_init(void);
 void fe_irc_notifylist_deinit(void);void fe_irc_dcc_deinit(void);
 void fe_irc_modules_init(void) {  fe_irc_dcc_init(); fe_irc_notifylist_init(); }
 void fe_irc_modules_deinit(void) { fe_irc_notifylist_deinit(); fe_irc_dcc_deinit();  }
+#endif
